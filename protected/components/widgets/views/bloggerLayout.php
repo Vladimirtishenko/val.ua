28e7dfbf4e09d6611c2b7a1a79889bc892bbbf2f
vladimirtishenko <vladimirtishenko1@gmail.com>
2017-01-07 22:02:22 +0200

    remove link from bloger

--- protected/components/widgets/views/bloggerLayout.php
+++ protected/components/widgets/views/bloggerLayout.php
@@ -12,8 +12,10 @@
         <img class="val-bloger-avatar" src="/uploads/users/avatars/<?=$blogger['user_avatar'];?>" alt="">
         <div class="val-bloger-info-description">
             <span class="val-content-news-data"><?= ($dateNow == date('Y-m-d', strtotime($blogger['article_date']))) ? date('H:i', strtotime($news->date)) : intval(date('d', strtotime($blogger['article_date']))).' '.Yii::app()->controller->getMonth($blogger['article_date']).' '.date('Y', strtotime($blogger['article_date'])); ?></span>
-            <a href="/blog/default/bloger/<?=$blogger['user_id']?>/uk.html" class="val-info-bloger-name"><?=$blogger['user_name'];?></a>
+            <a href="/" class="val-info-bloger-name"><?=$blogger['user_name'];?></a>
             <p class="val-info-bloger-prof"><?=$blogger['user_profession'];?></p>
         </div>
     </div>
 </div>
+
+<!-- blog/default/bloger/<?=$blogger['user_id']?>/uk.html -->

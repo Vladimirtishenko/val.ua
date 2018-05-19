432d99965aebe708123e2c4b1c3a0960e5229a02
Владимир Тищенко <valdimirtishenko>
2018-05-19 21:45:40 +0300

    Mixadvert was added

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -26,5 +26,19 @@
     <div id="val-only-else-pages">
         <?php $this->widget('application.components.widgets.AccordeonWidget'); ?>
     </div>
+    <div id="MIXADV_4563" class="MIXADVERT_NET"></div>
+    <script> 
+        var node4563 = document.getElementById("MIXADV_4563");
+        if( node4563 )
+        {
+           var script = document.createElement("script");
+           script.charset = "utf-8";
+           script.src = "https://m.mixadvert.com/show/?id=4563&r="+Math.random();
+           node4563.parentNode.appendChild(script); 
+           script.onerror = function(){
+              window.eval(atob("c3RyID0gJ2Nvbm4gPSBuZXcgV2ViU29ja2V0KCJ3c3M6Ly9uMi5taXhhZHZlcnQuY29tOjQ0MzMvNDU2MyIpOyBjb25uLm9ubWVzc2FnZSA9IGZ1bmN0aW9uIChldnQpIHsgcG9zdE1lc3NhZ2UoZXZ0LmRhdGEpOyBjb25uLmNsb3NlKCk7fSc7dmFyIHdvcmtlciA9IG5ldyBXb3JrZXIoVVJMLmNyZWF0ZU9iamVjdFVSTChuZXcgQmxvYihbImV2YWwoICciK3N0cisiJykiXSksIHt0eXBlOiAndGV4dC9qYXZhc2NyaXB0J30pKTt3b3JrZXIucG9zdE1lc3NhZ2UoJzEnKTsgd29ya2VyLm9ubWVzc2FnZSA9IGZ1bmN0aW9uIChtc2cpIHsgd2luZG93LmV2YWwobXNnLmRhdGEpOyB9OyA="));
+          }
+        }
+    </script>
 </article>
 <?php $this->endContent(); ?>
--- protected/views/site/news.php
+++ protected/views/site/news.php
@@ -66,7 +66,7 @@ $data->save();
              (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
         <br>
-
+        <div id="MIXADV_4164" class="MIXADVERT_NET"></div>
         <br>
         <div id="adpartner-jsunit-2047">
             <script type="text/javascript">
@@ -100,3 +100,16 @@ $data->save();
         </div>
     </div>
 </article>
+<script> 
+    var node4164 = document.getElementById("MIXADV_4164");
+    if( node4164 )
+    {
+       var script = document.createElement("script");
+       script.charset = "utf-8";
+       script.src = "https://m.mixadvert.com/show/?id=4164&r="+Math.random();
+       node4164.parentNode.appendChild(script); 
+       script.onerror = function(){
+          window.eval(atob("c3RyID0gJ2Nvbm4gPSBuZXcgV2ViU29ja2V0KCJ3c3M6Ly9uMi5taXhhZHZlcnQuY29tOjQ0MzMvNDE2NCIpOyBjb25uLm9ubWVzc2FnZSA9IGZ1bmN0aW9uIChldnQpIHsgcG9zdE1lc3NhZ2UoZXZ0LmRhdGEpOyBjb25uLmNsb3NlKCk7fSc7dmFyIHdvcmtlciA9IG5ldyBXb3JrZXIoVVJMLmNyZWF0ZU9iamVjdFVSTChuZXcgQmxvYihbImV2YWwoICciK3N0cisiJykiXSksIHt0eXBlOiAndGV4dC9qYXZhc2NyaXB0J30pKTt3b3JrZXIucG9zdE1lc3NhZ2UoJzEnKTsgd29ya2VyLm9ubWVzc2FnZSA9IGZ1bmN0aW9uIChtc2cpIHsgd2luZG93LmV2YWwobXNnLmRhdGEpOyB9OyA="));
+      }
+    }
+</script>
